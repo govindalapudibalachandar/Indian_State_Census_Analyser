4672927e3b6a07cd83b279a25e53ea8c1daa5c00
balachandar <govindalapudibalachandar@gmail.com>
2021-10-02 23:55:03 +0530

    Added:indianStateCensusCsvfile is added

---
+++ src/main/java/com/bridgelabz/CensusAnalyserException.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class CensusAnalyserException extends Exception {
+
+    public enum ExceptionType {
+        CENSUS_FILE_PROBLEM, RUN_TIME_EXCEPTION
+    }
+
+    public ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
